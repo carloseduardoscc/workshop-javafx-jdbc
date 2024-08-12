df55385c6af7c10efe6599f1be440b8d39857476
Carlos Eduardo <carloseduscc@gmail.com>
2024-08-11 23:09:39 -0300

    Local project created

---
+++ src/application/Main.java
@@ -0,0 +1,24 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        ClassLoader classLoader = Main.class.getClassLoader();
+        FXMLLoader fxmlLoader = new FXMLLoader(classLoader.getResource("gui/view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
