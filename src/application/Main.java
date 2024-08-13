ada0c7a096e9cc4feef8280a148dec1411396463
Carlos Eduardo <carloseduscc@gmail.com>
2024-08-13 07:48:47 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,16 +10,23 @@ import java.awt.*;
 import java.io.IOException;
 
 public class Main extends Application {
+
+    private static Scene mainScene;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getClassLoader().getResource("gui/MainView.fxml"));
         Parent parent = fxmlLoader.load();
-        Scene scene = new Scene(parent);
-        stage.setScene(scene);
+        mainScene = new Scene(parent);
+        stage.setScene(mainScene);
         stage.show();
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    public static Scene getMainScene (){
+        return mainScene;
     }
 }
